f26513e6ea3b7aec842681f41becd0cb9cf46037
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-14 23:58:31 +0100

    Implemented disaster
    - if random get 42 the bar closes immediately

--- src/main/java/licence/pro/models/Ia.java
+++ src/main/java/licence/pro/models/Ia.java
@@ -3,6 +3,7 @@ package licence.pro.models;
 import licence.pro.singletons.SBar;
 import licence.pro.singletons.SBeerStock;
 import licence.pro.singletons.SCustomers;
+import licence.pro.singletons.SRandom;
 import licence.pro.singletons.SWaiters;
 
 public class Ia implements Runnable {
@@ -17,33 +18,52 @@ public class Ia implements Runnable {
 				Thread.sleep(IA_TICK);
 
 				// Put some random disaster
-				
+				if (SRandom.getInstance().nextInt(43) == 42)
+					interupEverything();
+					
 				// If no more available beer, terminate all waiter thread
-				if (SBeerStock.getInstance().isOutOfOrder() && !this.waitersKilled) {				
-					 for (int i =  0 ; i < SWaiters.getInstance().length ; i++) 
-						 SWaiters.getInstance()[i].interrupt();
-					 
-					 System.out.println("Killed waiters threads");
-					 this.waitersKilled = true;
+				if (SBeerStock.getInstance().isOutOfOrder() && !this.waitersKilled) {									 
+					interupWaiters();					
 				}
 				
 				// if queue is empty and no more waiters
 				if (waitersKilled && (SBar.getInstance().getNbPeerPouredNotTook() < 1) ) {
-					for (int i =  0 ; i < SCustomers.getInstance().length ; i++) 
-						SCustomers.getInstance()[i].interrupt();
-					
-					System.out.println("Killed customers threads");
-					this.customersKilled = true;
+					interupCustomers();
 				}
 				
 				// If no customers or waiters, suicide of the ia
 				if (customersKilled && waitersKilled) {		
-					Thread.currentThread().interrupt();
-					System.out.println("Bar is closed");					
+					interupIa();		
 				}				
 			}         
 		}  catch (InterruptedException e) {
 			System.out.println("Bar is closing due to error");
 		}
+	}
+	
+	private void interupEverything() {
+		interupCustomers();
+		interupWaiters();
+		interupIa();
+	}
+	
+	private void interupCustomers() {
+		for (int i =  0 ; i < SCustomers.getInstance().length ; i++) 
+			SCustomers.getInstance()[i].interrupt();
+		
+		System.out.println("Killed customers threads");
+		this.customersKilled = true;
+	}
+	
+	private void interupWaiters() {
+		for (int i =  0 ; i < SWaiters.getInstance().length ; i++) 
+			 SWaiters.getInstance()[i].interrupt();
+		System.out.println("Killed waiters threads");
+		this.waitersKilled = true;
+	}
+	
+	private void interupIa() {
+		Thread.currentThread().interrupt();
+		System.out.println("Bar is closed");	
 	}
 }
