57793fb9158b2e0261c3a3890251146c18068377
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-14 22:42:10 +0100

    Constants
    - added project constants to centralize thoose parameter and better readability

---
+++ src/main/java/licence/pro/constants/ProjectConstants.java
@@ -0,0 +1,9 @@
+package licence.pro.constants;
+
+public class ProjectConstants {	
+	public static final int TICK_IA = 20;
+	public static final int TICK_WAITER = 100;
+	public static final int TICK_CUSTOMER = 100;	
+	public static final int NB_WAITERS = 3;
+	public static final int NB_CUSTOMERS = 2;
+}
