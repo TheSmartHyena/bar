d268a598ae7c018634e2715c5d1f13c577dde810
SkopalPhilippe <skopal.philippe@gmail.com>
2020-03-14 22:44:33 +0100

    Project constants
    - implemented constants into SWaiters and SCustomers

--- src/main/java/licence/pro/singletons/SCustomers.java
+++ src/main/java/licence/pro/singletons/SCustomers.java
@@ -1,17 +1,17 @@
 package licence.pro.singletons;
 
+import licence.pro.constants.ProjectConstants;
+
 public class SCustomers {
 	
 	// Named A, in case of if we want to add more waiter
 	private static Thread[] instance = null;
-	private static final int NB_CUSTOMERS = 3;
 	
 	public static Thread[] getInstance() {
 		if(instance == null) {
-			instance = new Thread[NB_CUSTOMERS];
-			for (int i=0; i<NB_CUSTOMERS; i++) {
-				instance[i] = new Thread(SCustomer.getInstance());
-			}
+			instance = new Thread[ProjectConstants.NB_CUSTOMERS];
+			for (int i=0; i<ProjectConstants.NB_CUSTOMERS; i++) 
+				instance[i] = new Thread(SCustomer.getInstance());			
 		}
 		return instance;
 	}
--- src/main/java/licence/pro/singletons/SWaiters.java
+++ src/main/java/licence/pro/singletons/SWaiters.java
@@ -1,17 +1,17 @@
 package licence.pro.singletons;
 
+import licence.pro.constants.ProjectConstants;
+
 public class SWaiters {
 	
 	// Named A, in case of if we want to add more waiter
 	private static Thread[] instance = null;
-	private static final int NB_WAITERS = 3;
 	
 	public static Thread[] getInstance() {
 		if(instance == null) {
-			instance = new Thread[NB_WAITERS];
-			for (int i=0; i<NB_WAITERS; i++) {
-				instance[i] = new Thread(SWaiter.getInstance());
-			}
+			instance = new Thread[ProjectConstants.NB_WAITERS];
+			for (int i=0; i<ProjectConstants.NB_WAITERS; i++)
+				instance[i] = new Thread(SWaiter.getInstance());			
 		}
 		return instance;
 	}
